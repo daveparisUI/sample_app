fcf1baf91c854142ac98699bbe484904ca372a39
Dave Paris <dave-paris@uiowa.edu>
2013-10-15 11:02:08 -0500

    just add SOMETHING to github already

--- spec/requests/user_pages_spec.rb
+++ spec/requests/user_pages_spec.rb
@@ -13,10 +13,10 @@ describe "UserPages" do
 
   describe "profile page" do
     # Code to make a user variable
-    let(:user) {FactoryGirl.create(:user)}
+    let(:user) { FactoryGirl.create(:user) }
     before { visit user_path(user) }
 
-    it { should have_selector('hi',        text: user.name) }
+    it { should have_selector('h1',        text: user.name) }
     it { should have_selector('title',     text: user.name) }
 
   end
